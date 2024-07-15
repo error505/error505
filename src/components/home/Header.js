0af41bc44ac57e541e3d56ebb63a802523510df1
Igor Iric <iric_i@hotmail.com>
2024-07-15 10:08:48 +0200

    Fixing home link

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -1,6 +1,7 @@
 // src/components/Header.js
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link as ScrollLink } from 'react-scroll';
 /* eslint-disable jsx-a11y/anchor-is-valid */
 const Header = () => {
   return (
@@ -8,21 +9,51 @@ const Header = () => {
       <a href="/error505" className="home-button open-sidebar1">
         <FontAwesomeIcon icon="home" /><span></span>
       </a>
-      <a href="#about" className="about-button open-sidebar1">
-        <FontAwesomeIcon icon="user" /><span></span>
-      </a>
-      <a href="#portfolio" className="portfolio-button open-sidebar1">
-        <FontAwesomeIcon icon="briefcase" /><span></span>
-      </a>
-      <a href="#services" className="service-button open-sidebar1">
-        <FontAwesomeIcon icon="dollar-sign" /><span></span>
-      </a>
-      <a href="#blog" className="blog-button open-sidebar1">
-        <FontAwesomeIcon icon="blog" /><span></span>
-      </a>
-      <a href="#contact" className="contact-button open-sidebar1">
-        <FontAwesomeIcon icon="envelope" /><span></span>
-      </a>
+      <ScrollLink
+        to="about"
+        smooth={true}
+        duration={500}
+        className="about-button open-sidebar1"
+      >
+        <FontAwesomeIcon icon="user" />
+        <span></span>
+      </ScrollLink>
+      <ScrollLink
+        to="portfolio"
+        smooth={true}
+        duration={500}
+        className="portfolio-button open-sidebar1"
+      >
+        <FontAwesomeIcon icon="briefcase" />
+        <span></span>
+      </ScrollLink>
+      <ScrollLink
+        to="services"
+        smooth={true}
+        duration={500}
+        className="services-button open-sidebar1"
+      >
+        <FontAwesomeIcon icon="dollar-sign" />
+        <span></span>
+      </ScrollLink>
+      <ScrollLink
+        to="blog"
+        smooth={true}
+        duration={500}
+        className="blog-button open-sidebar1"
+      >
+        <FontAwesomeIcon icon="blog" />
+        <span></span>
+      </ScrollLink>
+      <ScrollLink
+        to="contact"
+        smooth={true}
+        duration={500}
+        className="contact-button open-sidebar1"
+      >
+        <FontAwesomeIcon icon="envelope" />
+        <span></span>
+      </ScrollLink>
     </header>
   );
 };
